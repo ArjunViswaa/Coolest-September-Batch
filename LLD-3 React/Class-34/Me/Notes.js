157f348a01fcff41f0f979132e3b9d5d63dede4f
Arjun Viswaa <arjunviswaa@gmail.com>
2025-01-14 16:56:13 +0530

    Class 34 Me

---
+++ LLD-3 React/Class-34/Me/Notes.js
@@ -0,0 +1,14 @@
+// React is a JavaScript library for building user interfaces.
+// Untill 2011, Facebook was using Vanilla javascript, jQuery and other libraries to build user interfaces. But it was getting difficult to manage the codebase.
+// So in 2013, Facebook made it open source, and available for all developers around the world. Due to its popularity, it is now maintained by Facebook and a community of individual developers and companies.
+// Companies that use React include Facebook, Instagram, Netflix, Airbnb, WhatsApp, Dropbox, Twitter, Spotify, Amazon and more.
+// In 2011, Jordan Walke, a software engineer at Facebook, created a new library for building user interfaces.
+// React is used to build single page applications.
+// React allows us to create reusable UI components.
+
+/*
+Features : 
+1. Virtual DOM.
+2. Component based architecture.
+3. State Management better.
+*/
